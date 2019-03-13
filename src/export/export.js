29e7ae76c5ef8ae5716e2f7bcae408baeb832ba7
Mykal Machon <mykal.machon@student.ufv.ca>
2019-03-13 10:41:33 -0700

    Added Export Method #9
    + Added an export function that takes the hunter.io email response and pushes each email into its own company row with possible first name, last name, and phone numbers added in addition to the email.

--- src/export/export.js
+++ src/export/export.js
@@ -73,3 +73,65 @@ exports.exportDetailedData = data => {
   const csv = jsonParser.parse(rows);
   return csv;
 };
+
+exports.exportDetailedDataWithEmail = () => {
+  const headings = [
+    'First Name',
+    'Last Name',
+    'email',
+    'Phone Number',
+    'Mobile Number',
+    'Company Name',
+    'Street',
+    'City',
+    'Province',
+    'Country',
+    'Postal Code',
+    'Website'
+  ];
+  const rows = [];
+  data.forEach(obj => {
+    const locArr = obj.formatted_address.split(',');
+    obj.emails.forEach(email => {
+      let fn, ln, pn;
+      if (email.first_name != null && email.last_name != null) {
+        fn = email.first_name;
+        ln = email.last_name;
+      }
+      if (email.phone_number != null) {
+        pn = email.phone_number;
+      }
+      rows.push({
+        'First Name': fn || '',
+        'Last Name': ln || '',
+        email: email.value,
+        'Phone Number': obj.formatted_phone_number,
+        'Mobile Number': pn || '',
+        'Company Name': obj.name.trim(),
+        Street: locArr[0].trim(),
+        City: locArr[1].trim(),
+        Province: locArr[2].slice(0, 3).trim(),
+        Country: locArr[3].trim(),
+        'Postal Code': locArr[2].slice(3).trim(),
+        Website: obj.website
+      });
+    });
+    rows.push({
+      'First Name': '',
+      'Last Name': '',
+      email: '',
+      'Phone Number': obj.formatted_phone_number,
+      'Mobile Number': '',
+      'Company Name': obj.name.trim(),
+      Street: locArr[0].trim(),
+      City: locArr[1].trim(),
+      Province: locArr[2].slice(0, 3).trim(),
+      Country: locArr[3].trim(),
+      'Postal Code': locArr[2].slice(3).trim(),
+      Website: obj.website
+    });
+  });
+  const jsonParser = new JsonToCSVParser({ headings });
+  const csv = jsonParser.parse(rows);
+  return csv;
+};
